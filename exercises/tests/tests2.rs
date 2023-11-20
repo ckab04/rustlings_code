dbe4245e19107ad9ea09fe6b31bde5d09f0355c6
ckab04 <ciceronkapenabm@gmail.com>
2023-11-20 23:32:51 +0100

    Tests

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -6,12 +6,13 @@
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        let a = 36;
+        let b = 36;
+        assert_eq!(a, b);
     }
 }
