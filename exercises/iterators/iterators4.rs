2b48a03b9efc7a7ba3780cebe682dc46e29a9d72
ckab04 <ciceronkapenabm@gmail.com>
2023-11-25 23:50:04 +0100

    working on iterators (continued 2)

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -15,6 +15,14 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    if num == 0 || num == 1{
+        1
+    }else{
+        (1..=num).into_iter().product()
+    }
+
+
 }
 
 #[cfg(test)]
