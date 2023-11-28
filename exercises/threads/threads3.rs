7297b339a48700f2b07142f2b54615579f667f26
ckab04 <ciceronkapenabm@gmail.com>
2023-11-28 21:18:35 +0100

    Threads end

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::sync::mpsc;
 use std::sync::Arc;
@@ -31,18 +30,22 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
 
+    let mx = tx.clone();
+    //let rx : mpsc::Receiver<u32>;
+
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            mx.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 
+    let nx = tx.clone();
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            nx.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
